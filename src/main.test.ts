d0b4cfbeefa54ee94e759697ced413631a26b892
Nicolas W <nwadoux@myges.fr>
2023-12-05 15:19:17 +0100

    fix tests to handle Figure isFigure methods

--- src/main.test.ts
+++ src/main.test.ts
@@ -19,6 +19,48 @@ describe('init test', () => {
     });
 });
 
+describe('Yams Figures', () => {
+   it('brelan isFigure should return true if there is a brelan', () => {
+         const rollCounts = getRollByRepetition(brelan);
+         expect(FIGURES[0].isFigure(rollCounts)).toBe(true);
+   });
+
+    it('brelan isFigure should return false if there is no brelan', () => {
+        const rollCounts = getRollByRepetition([1, 2, 3, 4, 5]);
+        expect(FIGURES[0].isFigure(rollCounts)).toBe(false);
+    });
+
+    it('carre isFigure should return true if there is a carre', () => {
+        const rollCounts = getRollByRepetition(carre);
+        expect(FIGURES[1].isFigure(rollCounts)).toBe(true);
+    });
+
+    it('carre isFigure should return false if there is no carre', () => {
+        const rollCounts = getRollByRepetition([1, 2, 3, 4, 5]);
+        expect(FIGURES[1].isFigure(rollCounts)).toBe(false);
+    });
+
+    it('yams isFigure should return true if there is a yams', () => {
+        const rollCounts = getRollByRepetition(yams);
+        expect(FIGURES[2].isFigure(rollCounts)).toBe(true);
+    });
+
+    it('yams isFigure should return false if there is no yams', () => {
+        const rollCounts = getRollByRepetition([1, 2, 3, 4, 5]);
+        expect(FIGURES[2].isFigure(rollCounts)).toBe(false);
+    });
+
+    it('full isFigure should return true if there is a full', () => {
+        const rollCounts = getRollByRepetition([1, 1, 1, 2, 2]);
+        expect(FIGURES[3].isFigure(rollCounts)).toBe(true);
+    });
+
+    it('full isFigure should return false if there is no full', () => {
+        const rollCounts = getRollByRepetition([1, 2, 3, 4, 5]);
+        expect(FIGURES[3].isFigure(rollCounts)).toBe(false);
+    });
+});
+
 describe('Yams checkRollsValidity', () => {
     it('should throw an error if there is less than 5 rolls provided', () => {
         expect(() => {
