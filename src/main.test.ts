a27fde69db12926adec2910ee3f96ede08674caf
Nicolas W <nwadoux@myges.fr>
2023-12-05 14:30:05 +0100

    we need first to get number of repetions by numbers

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,12 +1,14 @@
-import {getPointsForRoll} from "./main";
+import {getPointsForRoll, getDiceValuesWithCount} from "./main";
 
 const brelan = [1, 2, 3, 2, 2];
 
-describe('Yams', () => {
+describe('init test', () => {
     it('should test', () => {
         expect(true).toBe(true);
     });
+});
 
+describe('Yams getPointsForRoll', () => {
     it('should throw an error if there is less than 5 rolls provided', () => {
         expect(() => {
             getPointsForRoll([1, 2, 3, 2]);
@@ -18,14 +20,25 @@ describe('Yams', () => {
             getPointsForRoll([1, 2, 3, 2, 2, 2]);
         }).toThrowError('You must provide 5 rolls');
     });
+});
 
-    it('should return 28 if there is a brelan of 2', () => {
-        expect(getPointsForRoll(brelan)).toBe(28);
-    });
-    it('should return 28 if there is a brelan of 3', () => {
-        expect(getPointsForRoll([1, 2, 3, 3, 3])).toBe(28);
-    });
-    it('should return 28 if there is a brelan of 4', () => {
-        expect(getPointsForRoll([1, 4, 3, 4, 4])).toBe(28);
+describe('Yams getDiceValuesWithCount', () => {
+    it('should return number of repetitions in the rolls', () => {
+        const rolls = [1, 2, 3, 2, 2];
+        const diceValuesWithCount = getDiceValuesWithCount(rolls);
+        expect(diceValuesWithCount).toEqual([
+            {
+                rollValue: 1,
+                repetitionCount: 1,
+            },
+            {
+                rollValue: 2,
+                repetitionCount: 3,
+            },
+            {
+                rollValue: 3,
+                repetitionCount: 1,
+            },
+        ]);
     });
 });
